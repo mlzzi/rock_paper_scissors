37cb10f39171a6e2a77d59b6a128254548a76807
mluzzi <muriloluzzi@gmail.com>
2021-04-21 10:12:24 -0300

    Function set with the rules of the game

--- script.js
+++ script.js
@@ -3,3 +3,23 @@ function computerPlay() {
     return words[ Math.floor(Math.random() * words.length)];
  }
 
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == "rock" && computerSelection == "paper") {
+        return "You lose! Paper beats Rock!";
+    } else if (playerSelection == "rock" && computerSelection == "scissors") {
+        return "You win! Rock beats Scissors!";
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
+        return "You win! Paper beats Rock!";
+    } else if (playerSelection == "paper" && computerSelection == "scissors") {
+        return "You lose! Scissors beats Paper!";
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        return "You win! Scissors beats Paper!";
+    } else if (playerSelection == "scissors" && computerSelection == "rock") {
+        return "You lose! Rock beats Scissors!";
+    }
+    return "This round tied, try again!"
+ }   
+ 
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
